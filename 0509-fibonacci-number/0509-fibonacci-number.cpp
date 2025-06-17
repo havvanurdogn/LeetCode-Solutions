7c662ec363d6c75d5a03319b68febcee74bd1d92
havvanurdogn <128641511+havvanurdogn@users.noreply.github.com>
2025-06-18 02:28:04 +0300

    Time: 0 ms (100%), Space: 8.1 MB (17.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        
         if (n == 0 || n == 1){
-                return n;
-        }
-        int prev2 = 0;
-        int prev = 1;
-        
-        for (int i = 2; i <= n ; i++){
-            int curri = prev2 + prev;
-            prev2 = prev;
-            prev = curri;
+            return n;
         }
 
-        return prev;
+        int* dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
 
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int result = dp[n];
+        delete[] dp;
+        return result;
     }
 };
